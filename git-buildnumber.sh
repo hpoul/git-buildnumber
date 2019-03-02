0bdb33a437a3a3f26b59337d4788c60bdd839939
Herbert Poul <herbert@poul.at>
2019-03-02 21:32:59 +0200

    write our own tree referencing build number to commit.

--- git-buildnumber.sh
+++ git-buildnumber.sh
@@ -12,7 +12,7 @@
 #
 
 #set -xeu
-set -eu
+set -euE
 
 VERSION=1.0
 
@@ -20,6 +20,7 @@ REMOTE=origin
 
 REFS_BASE=refs/buildnumbers
 REFS_LAST=${REFS_BASE}/last
+REFS_COMMITS=${REFS_BASE}/commits
 REFS_NOTES=refs/notes/buildnumbers
 REFSPEC="+${REFS_BASE}/*:${REFS_BASE}/* +refs/notes/*:refs/notes/*"
 
@@ -44,23 +45,34 @@ function check_existing_buildnumber () {
 function find_commit_by_buildnumber {
     buildnumber=$1
 
-    hash=`echo "$buildnumber" | git hash-object --stdin`
-    notesfile=`git ls-tree $REFS_NOTES | grep "blob ${hash}" | cut -f 2`
+    blobhash=`git ls-tree $REFS_COMMITS "b${buildnumber}" | cut -f 1 | cut -d' ' -f3`
+    commits=`git cat-file blob $blobhash`
 
-    test -z "$notesfile" && fail "Unable to find commit for build number ${buildnumber}"
+    unique=`echo "$commits" | uniq`
+    
+    _logi "Found the following commits: \n$unique\n"
 
-    git log "$notesfile" -1
+    git log $commits -1
+
+    # hash=`echo "$buildnumber" | git hash-object --stdin`
+    # notesfile=`git ls-tree $REFS_NOTES | grep "blob ${hash}" | cut -f 2`
+
+    # test -z "$notesfile" && fail "Unable to find commit for build number ${buildnumber}"
+
+    # git log "$notesfile" -1
 }
 
-function force_buildnumber {
+function force_buildnumber {    
     buildnumber=$1
+    _fetch
     _write_buildnumber $buildnumber "forced"
     echo "Written build number."
     _push
 }
 
 function log {
-    tail `git rev-parse --git-dir`/logs/${REFS_LAST}
+    # tail `git rev-parse --git-dir`/logs/${REFS_LAST}
+    git log ${REFS_COMMITS}
 }
 
 function usage {
@@ -76,13 +88,64 @@ function usage {
     echo "                          the current commit."
 }
 
+function _logt {
+    echo -e "\e[2m  TRACE $* \e[0m" >&2
+}
+
+function _logd {
+    echo -e "\e[34m  DEBUG $* \e[0m" >&2
+}
+
+function _logi {
+    echo -e "\e[33m$*\e[0m" >&2
+}
+
 function _write_buildnumber {
     buildnumber=$1
     reason=${2}
-    set -x
+
+    message="buildnumber: ${buildnumber} (${reason}) at commit `git show-ref -s HEAD`"
     buildnumberhash=`echo "${buildnumber}" | git hash-object -w --stdin`
-    git update-ref -m "buildnumber: ${buildnumber} (${reason})" --create-reflog ${REFS_LAST} ${buildnumberhash} `git show-ref -s refs/buildnumbers/last`
+    git update-ref -m "${message}" --create-reflog ${REFS_LAST} ${buildnumberhash} `git show-ref -s refs/buildnumbers/last`
     ${CMD_NOTES} add -m "${buildnumber}" -f HEAD
+
+    _logd "writing our own commits log"
+
+    # For fun (and to have our own git log) create our own 
+    # tree and commit in $REFS_COMMITS
+    treefile=`mktemp`
+    buildnumberfile=`mktemp`
+    buildnumberfilename="b${buildnumber}"
+    commitshash=`git show-ref -s $REFS_COMMITS || :`
+    parent=""
+    _logt "commitshash: $commitshash\n\n"
+    if test -n "$commitshash" ; then
+        parent="-p $commitshash"
+        git ls-tree $commitshash | grep -v "\t${buildnumberfilename}$" > $treefile || :
+        previous=`git ls-tree $commitshash ${buildnumberfilename} | cut -f1 | cut -d' ' -f3`
+        _logt "previous hash for ${buildnumberfilename} is '${previous}'"
+        if test -n "$previous" ; then
+            # another commit already has this build number.. but anyway..
+            _logd "Another commit ($previous) already uses this."
+            git cat-file blob "$previous" > $buildnumberfile
+        fi
+    fi
+    _logt "buildnumber file at $buildnumberfile"
+    git show-ref -s HEAD >> $buildnumberfile
+    buildnumberfilehash=`git hash-object -w -- "$buildnumberfile"`
+    
+    _logt "Creating tree at $treefile"
+    echo -e "100644 blob ${buildnumberfilehash}\t${buildnumberfilename}" >> $treefile
+    treehash=`cat "$treefile" | git mktree`
+    newcommitshash=`git commit-tree $parent $treehash -m "${message}"`
+    git update-ref -m "${message}" --create-reflog ${REFS_COMMITS} ${newcommitshash}
+
+    rm $treefile $buildnumberfile
+
+}
+
+function _fetch {
+    git fetch -q $REMOTE ${REFSPEC}
 }
 
 function _push {
@@ -94,7 +157,10 @@ git diff-index --quiet HEAD || fail "Requires a clean repository state, without 
 case "${1:-generate}" in
     generate) # proceed with finding next build number
     ;;
-    find-commit)
+    fetch) _fetch && exit 0 ;;
+    push) _push && exit 0 ;;
+    sync) _fetch && _push && exit 0 ;;
+    find | find-commit)
         test -z "$2" && usage && fail
         find_commit_by_buildnumber "$2"
         exit 0
@@ -105,6 +171,7 @@ case "${1:-generate}" in
         exit 0
     ;;
     log) log && exit 0 ;;
+    help) usage && exit 1 ;;
     *)
         usage
         fail "Unknown argument ($*)"
@@ -116,7 +183,7 @@ esac
 
 check_existing_buildnumber
 
-git fetch -q $REMOTE ${REFSPEC}
+_fetch
 
 check_existing_buildnumber
 
